bfb43e5001d7d3083dc130863e1030739756af93
VerbiteAI <dev@verbite.com>
2025-06-02 15:34:43 +0200

    responsive dev prod config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
